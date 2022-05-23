51a7ced9ab416c61585d05c06d46c88779d55b35
Leo Bowen Wang <leobwang@gmail.com>
2022-05-23 15:18:09 +0200

    list_menu()

--- main.py
+++ main.py
@@ -20,10 +20,43 @@
 # that then can be used in machine learning and deep learning applications.
 # Using a digital camera, firstly take photographs of 10 different objects to form the initial image dataset.
 # These then form the image dataset that can be read by the Python script.
+import sys
 
-def print_hi(name):
-    print(f'Hi, {name}')
+
+def list_menu():
+    print("************Image Augmentation Workshop Project**************")
+    choice = input("""
+        1: List the names of the available images.
+        2: Read a given RGB colour image using the image file name in order to select the image.
+        3: Convert the image to grayscale.
+        4: Convert the image to a black & white image where the RGB colour image is initially converted to a grayscale image.
+        5. Adjust the individual red, green and blue values for the pixels in the image with a number from 0 to 255.
+        6: View the original and modified image using Matplotlib.
+        7: Save the modified image with a given image file name.
+        8: Quit the Python script run.
+        Please enter your choice: """)
+
+    if choice == "1":
+        print("1")
+        list_menu()
+    elif choice == "8":
+        sys.exit()
+    else:
+        print("You must only select either a number from 1 - 8")
+        print("Please try again")
+        list_menu()
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    list_menu()
+
+
+def list_names():
+    import os
+
+    path = './images'
+
+    files = os.listdir(path)
+
+    for f in files:
+        print(f)
