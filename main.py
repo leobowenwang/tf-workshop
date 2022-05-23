7c08815f93a4f0a427ae5df0280efa302e1e316a
Leo Bowen Wang <leobwang@gmail.com>
2022-05-23 15:23:31 +0200

    reformat list

--- main.py
+++ main.py
@@ -23,18 +23,20 @@
 import sys
 import os
 
+
 def list_menu():
     print("************Image Augmentation Workshop Project**************")
-    choice = input("""
-        1: List the names of the available images.
-        2: Read a given RGB colour image using the image file name in order to select the image.
-        3: Convert the image to grayscale.
-        4: Convert the image to a black & white image where the RGB colour image is initially converted to a grayscale image.
-        5. Adjust the individual red, green and blue values for the pixels in the image with a number from 0 to 255.
-        6: View the original and modified image using Matplotlib.
-        7: Save the modified image with a given image file name.
-        8: Quit the Python script run.
-        Please enter your choice: """)
+    choice = input(
+        """1: List the names of the available images.
+2: Read a given RGB colour image using the image file name in order to select the image.
+3: Convert the image to grayscale.
+4: Convert the image to a black & white image where the RGB colour image is initially converted to a grayscale image.
+5. Adjust the individual red, green and blue values for the pixels in the image with a number from 0 to 255.
+6: View the original and modified image using Matplotlib.
+7: Save the modified image with a given image file name.
+8: Quit the Python script run.
+
+Please enter your choice: """)
 
     if choice == "1":
         print("1")
