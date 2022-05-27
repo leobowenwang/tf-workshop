6d7ebf6c07d160328ef46ccd90a123a1e385b879
Leo Bowen Wang <leobwang@gmail.com>
2022-05-27 16:39:57 +0200

    Header comment

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-# Write a Python script with TensorFlow and Matplotlib to implement image reading, augmentation and
-# saving functions, and to allow the user to manipulate the images using the Python input() function
-# (ref: https://www.w3schools.com/python/python_user_input.asp ). The Python script is to output suitably
-# formatted text to the display. A simple menu system is to be created to allow the user to:
-
-# Here, image augmentation is a technique that is used to artificially expand a dataset of available images
-# that then can be used in machine learning and deep learning applications.
-# Using a digital camera, firstly take photographs of 10 different objects to form the initial image dataset.
-# These then form the image dataset that can be read by the Python script.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By  : Leo Wang
+# Created Date: 27.05.2022
+# ---------------------------------------------------------------------------
+"""Image Augmentation Coding Project"""
+# ---------------------------------------------------------------------------
 import sys
 import time
 import os
